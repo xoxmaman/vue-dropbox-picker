3b4c7d6bd79213d1f3dc628eeff5b4c7604ef3bd
Kobyzskoi Dmytryi <d.kobizskoy@gmail.com>
2019-05-27 16:35:07 +0300

    docs

--- README.md
+++ README.md
@@ -1,29 +1,71 @@
 # vue-dropbox-picker
 
-## Project setup
+## Installation
+`yarn add vue-dropbox-picker` or `npm install vue-dropbox-picker --save`
+
+##Usage
 ```
-yarn install
+<VueDropboxPicker
+        :api-key="dropboxApiKey"
+        link-type="direct"
+        :multiselect="false"
+        :extensions="['.mp4']"
+        :folderselect="false"
+        :sizeLimit="1024"
+        @cancel="onCancel"
+        @picked="onPicked">
+    Open Dropbox Picker
+</VueDropboxPicker>
 ```
 
-### Compiles and hot-reloads for development
 ```
-yarn run serve
-```
+onCancel = () => {
+  console.log('onCancel')
+}
 
-### Compiles and minifies for production
-```
-yarn run build
-```
+onPicked = (files) => {
+  files.forEach((file) => {
+      console.log(file)
+  })
+}
+/*
+{
+    // Unique ID for the file, compatible with Dropbox API v2.
+    id: "id:...",
 
-### Run your tests
-```
-yarn run test
-```
+    // Name of the file.
+    name: "filename.txt",
 
-### Lints and fixes files
-```
-yarn run lint
-```
+    // URL to access the file, which varies depending on the linkType specified when the
+    // Chooser was triggered.
+    link: "https://...",
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+    // Size of the file in bytes.
+    bytes: 464,
+
+    // URL to a 64x64px icon for the file based on the file's extension.
+    icon: "https://...",
+
+    // A thumbnail URL generated when the user selects images and videos.
+    // If the user didn't select an image or video, no thumbnail will be included.
+    thumbnailLink: "https://...?bounding_box=75&mode=fit",
+
+    // Boolean, whether or not the file is actually a directory
+    isDir: false,
+}
+ */
+```
+### Props
+`link-type` - `direct` or `preview`. Default is `preview`. Type of returning URLs to chosen files <br>
+`multiselect` - Boolean. Possibility to choose several files<br>
+`extensions` - List of allowed extensions. Empty by default (allowed all files).<br>
+`folderselect` - Boolean. Possibility to choose folders
+`sizeLimit` - Max size of files. If unset - no limit
+
+### Events
+`cancel` - When picker is closed. No arguments<br>
+`picked` - When files are chosen. Argument - files (Array of file objects)<br>
+
+
+For more info - https://www.dropbox.com/developers/chooser
+
--- src/DropboxPicker.vue
+++ src/DropboxPicker.vue
@@ -1,13 +1,98 @@
 <template>
-  <div></div>
+  <div v-if="scriptLoaded && dropboxChooserIsSupported" @click="openChooser">
+      <slot/>
+      <button v-if="!this.$slots.default">Open dropbox picker</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue, Prop } from 'vue-property-decorator'
+import ChooserOptions = Dropbox.ChooserOptions
 
 @Component({})
-export default class App extends Vue {
+export default class DropboxPicker extends Vue {
+  @Prop({
+    required: true,
+  })
+  private apiKey!: string
 
+  @Prop({
+    required: false,
+    default: 'preview',
+  })
+  private linkType!: 'preview' | 'direct'
+
+  @Prop({
+    required: false,
+    default: false,
+  })
+  private multiselect!: boolean
+
+  @Prop({
+    required: false,
+    default: () => [],
+  })
+  private extensions!: string[]
+
+  @Prop({
+    required: false,
+    default: false,
+  })
+  private folderselect!: boolean
+
+  @Prop({
+    required: false,
+    default: 0,
+  })
+  private sizeLimit!: number
+
+  private scriptLoaded = false
+  private dropboxChooserIsSupported = false
+
+  private mounted(): void {
+    if (window.Dropbox) {
+      this.scriptLoaded = true
+    } else {
+      const dropBoxScript = document.createElement('script')
+      dropBoxScript.onload = () => {
+        this.scriptLoaded = true
+        this.dropboxChooserIsSupported = window.Dropbox!.isBrowserSupported()
+
+        if (!this.dropboxChooserIsSupported) {
+          console.warn('VueDropboxPicker: This browser is not supported')
+        }
+      }
+      dropBoxScript.setAttribute('src', 'https://www.dropbox.com/static/api/2/dropins.js')
+      dropBoxScript.setAttribute('id', 'dropboxjs')
+      dropBoxScript.setAttribute('data-app-key', this.apiKey)
+      document.head.appendChild(dropBoxScript)
+    }
+  }
+
+  private openChooser(): void {
+    const options: ChooserOptions = {
+      multiselect: this.multiselect,
+      folderselect: this.folderselect,
+      linkType: this.linkType,
+      success: (files: ReadonlyArray<Dropbox.ChooserFile>) => {
+        this.$emit('picked', files)
+      },
+      cancel: () => {
+        this.$emit('cancel')
+      },
+    }
+
+    if (this.extensions.length) {
+      options.extensions = this.extensions
+    }
+
+    if (this.sizeLimit) {
+      options.sizeLimit = this.sizeLimit
+    }
+
+    window.Dropbox!.choose(options)
+  }
 }
 </script>
+
+<style></style>

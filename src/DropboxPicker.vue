3c85fe1ad725f063e879bc53ac3e4a1a644ac6fc
Kobyzskoi Dmytryi <d.kobizskoy@gmail.com>
2019-05-27 16:33:43 +0300

    init

---
+++ src/DropboxPicker.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from './components/HelloWorld.vue';
+
+@Component({})
+export default class App extends Vue {
+
+}
+</script>
